192d027737e40bb68e612526f371e39eb7a54dd0
Weply <146603597+WeplyStudio@users.noreply.github.com>
2026-04-12 15:36:44 +0700

    Update pricing-section.tsx

--- components/sections/pricing-section.tsx
+++ components/sections/pricing-section.tsx
@@ -214,7 +214,7 @@ export function PricingSection() {
                 </ul>
               </div>
 
-              <link href="#contact"><button
+             <button onClick={() => window.location.href = '#contact'
                 className={`w-full py-4 md:py-5 rounded-xl md:rounded-2xl text-[9px] font-bold uppercase tracking-[0.3em] transition-all ${
                   index === 1
                     ? "bg-black text-white hover:bg-blue-600 shadow-xl"
@@ -222,7 +222,7 @@ export function PricingSection() {
                 }`}
               >
                 {index === 1 ? "Get Started" : index === 2 ? "Contact Me" : "Choose Plan"}
-              </button></link>
+              </button>
             </div>
           ))}
 
