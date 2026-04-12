1667d7cb9866eb1f58dcfea7ac7191ba912178e3
Weply <146603597+WeplyStudio@users.noreply.github.com>
2026-04-12 15:30:59 +0700

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {
 export function Navbar({ onMenuToggle, isMenuOpen }: NavbarProps) {
   return (
     <nav className="fixed top-0 w-full z-[350] px-8 py-10 flex justify-between items-center mix-blend-difference">
-      <a href="#" className="text-xs md:text-sm font-bold tracking-tighter hover:opacity-50 transition">
+      <a href="#" className="text-xl md:text-sm font-bold tracking-tighter hover:opacity-50 transition">
         Jasonn.doc™
       </a>
 
